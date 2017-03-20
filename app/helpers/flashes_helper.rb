3471341dc892e23def0db646e79016dc4e577245
yaoyizhi <email@yaoadou888>
2017-03-20 08:36:43 +0800

    create groups index

--- app/helpers/flashes_helper.rb
+++ app/helpers/flashes_helper.rb
@@ -5,6 +5,6 @@ module FlashesHelper
     end
 
     def user_facing_flashes
-        flash.to_harsh.slice 'alert', 'notice', 'warning'
+        flash.to_hash.slice 'alert', 'notice', 'warning'
     end
 end
