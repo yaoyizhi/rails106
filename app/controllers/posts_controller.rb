536b2d903b2940648ab4beb1fede20e6b6d8a73c
yaoyizhi <email@yaoadou888>
2017-03-22 08:30:30 +0800

    implement account/groups&posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -18,6 +18,31 @@ class PostsController < ApplicationController
       end
   end
 
+    def edit
+        @group = Group.find(params[:group_id])
+        @post = Post.find(params[:id])
+    end
+
+    def update
+        @group = Group.find(params[:group_id])
+
+        # @post.user = current_user
+        @post = Post.find(params[:id])
+        if @post.update(post_params)
+            redirect_to account_posts_path
+            flash[:notice] = 'Update Success'
+        else
+            render :edit
+        end
+    end
+
+    def destroy
+        @group = Group.find(params[:group_id])
+        @post = Post.find(params[:id])
+        @post.destroy
+        redirect_to account_posts_path
+    end
+
     private
 
     def post_params
